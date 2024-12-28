d4237720fe695caf16d55909c57f0052079d85be
Alexis Rossfelder <alexis@liteapp.fr>
2024-12-28 23:14:05 +0100

    Add color definitions and global font styles for TT Mussels Stencil Trl

---
+++ src/style/Colors.ts
@@ -0,0 +1,21 @@
+const colors = {
+  primary: [255, 102, 0],
+  "on-primary": [0, 0, 0],
+  secondary: [0, 102, 102],
+  "on-secondary": [0, 0, 0],
+  text: [255, 255, 255],
+  background: [0, 0, 0],
+};
+
+const computed_hex = (color: number[]) => "#" +
+  color.map((c) => c.toString(16).padStart(2, "0")).join("");
+const computed_rgb = (color: number[]) => color.join(", ");
+
+
+const computed_colors: {
+  [key: string]: string;
+} = Object.fromEntries(
+  Object.entries(colors).map(([key, value]) => ["color-" + key, computed_hex(value)]).concat(Object.entries(colors).map(([key, value]) => ["color-" + key + "-rgb", computed_rgb(value)]))
+);
+
+export default computed_colors;
