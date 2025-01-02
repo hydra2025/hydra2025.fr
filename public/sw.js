a5a205220e9dfa26936aa61062b4a675eac8afe0
Alexis Rossfelder <alexis@liteapp.fr>
2025-01-02 17:58:19 +0100

    Refactor component imports, enhance layout structure, and add new layouts for improved design consistency

---
+++ public/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "event-countdown-v1";
+const urlsToCache = [];
+
+self.addEventListener("install", (event) => {
+  console.log("Service Worker installing.");
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Service Worker caching app shell.");
+      cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
+self.addEventListener("push", function (event) {
+  if (event.data) {
+    const pushData = event.data.json();
+    const options = {
+      body: pushData.body,
+      icon: "/logo-192.png",
+      badge: "/logo-192.png",
+    };
+
+    event.waitUntil(
+      self.registration.showNotification(pushData.title, options)
+    );
+  }
+});
+
+self.addEventListener("notificationclick", function (event) {
+  event.notification.close();
+  event.waitUntil(clients.openWindow("/"));
+});
