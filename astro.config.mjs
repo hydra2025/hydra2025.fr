9d3186797fb3cdef107f0db14dbcd2f240643ec2
Alexis Rossfelder <alexis@liteapp.fr>
2025-01-05 21:44:00 +0100

    Add astro-compress integration and enhance input handling in FloppyBoat component for improved gameplay experience

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from "astro/config";
 
 import vue from "@astrojs/vue";
 
+import compress from "astro-compress";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  integrations: [vue(), compress()]
 });
