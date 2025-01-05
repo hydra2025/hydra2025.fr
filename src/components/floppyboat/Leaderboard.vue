640ab9a7b745a5c229051aafb79993c5876c7dda
Alexis Rossfelder <alexis@liteapp.fr>
2025-01-05 16:46:15 +0100

    Add Floppy Boat game component and leaderboard integration

---
+++ src/components/floppyboat/Leaderboard.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="scores">
+        <template v-for="(score, index) in leaderboard"><span class="rank">{{ index + 1 }}</span><span class="name">{{ score?.name }}</span><span
+                class="score">{{
+                    score?.score }}</span></template>
+    </div>
+</template>
+<script setup lang="ts">
+
+const ENDPOINT = "https://api.hydra2025.fr/floppyboat/leaderboard"
+type Score = {
+    score: number
+    name: string
+}
+
+const leaderboard: Ref<(Score | null)[]> = ref<(Score | null)[]>(Array.from({ length: 10 }).map((e) => null))
+let refreshInterval: number | null = null
+const refresh = () => {
+    if (import.meta.env.SSR) { clearInterval(refreshInterval!!); return; }
+    fetch(ENDPOINT).then(
+        (response: Response) => response.json().catch((error) => console.error(error))
+    ).then((scores: Score[]) => leaderboard.value = scores).catch((error) => console.error(error))
+}
+refreshInterval = setInterval(refresh, 30000)
+refresh()
+
+
+</script>
+
+
+<script lang="ts">
+import { defineComponent, ref, type Ref } from 'vue';
+export default defineComponent(
+    {
+        name: "Leaderboard"
+    }
+);
+</script>
+
+<style lang="css" scoped>
+.scores {
+    display: grid;
+    grid-template-columns: 2ch 16ch 3ch;
+    gap: 0 1rem;
+
+
+}
+
+.rank {
+    columns: 1;
+    margin: auto;
+    margin-right: 0;
+    font-family: var(--font-family-title);
+
+}
+
+.name {
+    columns: 2;
+    margin: auto;
+    font-family: var(--font-family-title);
+}
+
+.score {
+    columns: 3;
+    margin: auto;
+    font-family: var(--font-family-title);
+    color: var(--color-primary);
+}
+</style>
