0d0b727c6c8183f1f4db4004894a8ae16b72b548
Alexis Rossfelder <alexis@liteapp.fr>
2025-01-07 19:03:10 +0100

    Add clip page with video integration and update navigation buttons

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@
 import { defineConfig } from "astro/config";
 
 import vue from "@astrojs/vue";
-
 import compress from "astro-compress";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://hydra2025.fr",
-  integrations: [vue(), compress(), sitemap()],
+  integrations: [vue(), sitemap(), compress()],
 });
